7ddcaf695176a7d2a0118a3b986c8d62f4aa39b3
salim <salim.skander@ynov.com>
2025-04-22 16:00:05 +0200

    error deploy fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useRef, useState } from 'react';
+import { useRef } from 'react';
 import { motion, useScroll, useTransform, useInView, AnimatePresence } from 'framer-motion';
 import Link from 'next/link';
 import NeonEffect from '@/components/ui/NeonEffect';
